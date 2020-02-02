7703441eb94f621cef3224d7d63e696b8aaa87b8
meredithxu <yiyangxu@umail.ucsb.edu>
2020-02-02 00:11:43 -0800

    removed unused variable

--- src/components/Graph/graph.js
+++ src/components/Graph/graph.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CanvasJSReact from '../../canvasjs.react';
 
-var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class Graph extends Component {
