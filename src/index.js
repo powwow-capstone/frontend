cc168bceae5bbef4c06214bc024ffac0ba3a7b04
Bryan Wu <bryanwu64@gmail.com>
2020-02-16 20:18:33 -0800

    Revert "Merge bryan firebase"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './components/App';
-import Firebase, { FirebaseContext } from './components/Firebase';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-    <FirebaseContext.Provider value={new Firebase()}>
-        <App/>
-    </FirebaseContext.Provider>, 
-    document.getElementById('root')
-    );
+ReactDOM.render(<App />, document.getElementById('root'));
 
