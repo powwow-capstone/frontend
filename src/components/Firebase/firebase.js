eb6ba08edf5971c625bc784acf83a7455b2d5143
Bryan Wu <bryanwu64@gmail.com>
2020-02-16 18:44:09 -0800

    Finish react firebase auth p1

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const prodConfig = {
     apiKey: process.env.REACT_APP_PROD_API_KEY,
@@ -32,6 +33,8 @@ const config =
       app.initializeApp(config);
 
       this.auth = app.auth();
+      this.db = app.database();
+
     }
 
     // *** Auth API ***
@@ -48,5 +51,9 @@ const config =
 
     doPasswordUpdate = password =>
       this.auth.currentUser.updatePassword(password);
+
+    // *** User API ***
+    user = uid => this.db.ref(`users/${uid}`);
+    users = () => this.db.ref('users');
   }
   export default Firebase;
