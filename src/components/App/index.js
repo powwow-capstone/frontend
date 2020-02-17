2ca03b0ae3654996d98a5c8cedd516a03570e23f
Bryan Wu <bryanwu64@gmail.com>
2020-02-16 16:45:50 -0800

    Add navigation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import '../../css/App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from '../Home';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MainLoading from '../../MainLoading';
- 
-const App = () => (     
-      <BrowserRouter>
-      <div>
-        <MainLoading/>
-        <Switch>
-          <Route path="/" component={Home} exact/>
-        </Switch>
-      </div> 
-    </BrowserRouter>
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
+
+const App = () => (
+  <Router>
+    <div>
+      <MainLoading/>
+      <Navigation />
+      <hr />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
 );
+
 
 export default App;
