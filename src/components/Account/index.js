8eb58d370c7502698f3fbc3a07d61b1b055395de
Bryan Wu <bryanwu64@gmail.com>
2020-02-18 15:23:43 -0800

    Remove extra socials

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -16,14 +16,6 @@ const SIGN_IN_METHODS = [
     id: 'google.com',
     provider: 'googleProvider',
   },
-  {
-    id: 'facebook.com',
-    provider: 'facebookProvider',
-  },
-  {
-    id: 'twitter.com',
-    provider: 'twitterProvider',
-  },
 ];
 
 const AccountPage = ({ authUser }) => (
